23e209de096ab59e9f8173c01bb62efa2e4a1329
UlugbekgoldTASHKENT <ulugbekmirzaliyev77@gmail.com>
2022-12-02 20:53:41 +0500

    asa

---
+++ style.js
@@ -0,0 +1,33 @@
+// function addi(a){
+// return(a + 1)
+// }
+// console.log(addi(2));
+
+
+// function addi(a){
+//     return(a * 60)
+//     }
+//     console.log(addi(5));
+
+
+
+
+
+// function addi(a,h){
+//     return(a*h/2)
+//     }
+//     console.log(addi(3, 2));
+
+
+
+// function addi(a,h){
+//     return(a + h)
+//     }
+//     console.log(addi(3, 2));
+
+
+
+
+
+
+
